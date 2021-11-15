bbafe5321f30c43258ac7ce77123b3f89d76f8aa
Terror666 <ckuthulu@gmail.com>
2021-11-15 00:59:22 -0600

    trying to deploy

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,30 +7,36 @@ function Project() {
             name: 'NoteTaker',
             description: 'A simple app that lets you take notes and saves them to a database',
             language: 'JavaScript, MongoDB',
-            alt: 'Notepad'
+            alt: 'Notepad',
+            link: 'https://allthenotes.herokuapp.com'
         },
         {
             name: 'BudgetTracking',
             description: 'An app that allows you to track spending and deposits online and offline',
             language: 'PWA, Service worker, and Javascript',
-            alt: 'A line graph showing spending'
+            alt: 'A line graph showing spending',
+            link: 'https://fast-escarpment-53346.herokuapp.com'
         },
         {
             name: 'ThisThat',
             description: 'A fun game that gives you a series of choices to select from and you create your own',
             language: 'Handlebars.js, SQL, Javascript, CSS, HTML, Express server',
-            alt: 'This or That'
+            alt: 'This or That',
+            link: 'https://this--that.herokuapp.com'
         },
         {
             name: 'CovidRiskTracker',
             description: 'Search for events and covid risk information by state',
             language: 'HTML, CSS, JavaScript, API(ticketmaster, covid risk tracker)',
-            alt: 'Screenshot of covid risk tracker site'
+            alt: 'Screenshot of covid risk tracker site',
+            link: 'https://bpkaufman4.github.io/Group-2-project-1/'
         }
     ])
+
     return (
         <div className="box">
             {projects.map(project => (
+                <a href={project.link} target="_blank" className="link-text" rel="noreferrer">
                 <div className="card">
                     <img src={require(`../../assets/${project.name}.jpg`).default} alt={project.alt} key={project.name} className="image"/>
                     <div className="container">
@@ -39,6 +45,7 @@ function Project() {
                     <span className="a">{project.language}</span>
                     </div>
                 </div>
+                </a>
             ))}
         </div>
     )
